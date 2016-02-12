206768e45b47b1c3e73a501be263fe7e1016c059
Erwin de Haan <git@erayan.eu>
2016-02-12 14:31:31 +0100

    FTMS Project updates

--- gource.bat
+++ gource.bat
@@ -1,4 +1,4 @@
 @echo off
 title Gource FTMS
-"../gource/gource.exe" --fullscreen -1920x1080 --stop-at-end --file-idle-time 6000 --multi-sampling --key --max-files 0 --seconds-per-day 6
+"../gource/gource.exe" --fullscreen -3840x2160 --stop-at-end --file-idle-time 6000 --multi-sampling --key --max-files 0 --seconds-per-day 6
 pause
